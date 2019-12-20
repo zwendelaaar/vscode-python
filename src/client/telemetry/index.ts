6846e5badaa691fe7222841f38cd2721c207ce61
Don Jayamanne <don.jayamanne@yahoo.com>
2019-12-19 16:04:57 -0800

    Add ability to retry starting a session when starting a session fails (#9227)
    For #9187
    
    When starting a new session fails (e.g. due to bunked kernel or the like), allow user to recover by starting a new session with a different kernel.
    
    * Add ability to start retry starting sessions with different kernel
    * Fix message
    * Fixes
    * Address code review comments
    * Fix tests
    * Update labels

--- src/client/telemetry/index.ts
+++ src/client/telemetry/index.ts
@@ -1641,4 +1641,11 @@ export interface IEventNamePropertyMapping {
          */
         promptedToSelect?: boolean;
     };
+    /**
+     * Telemetry event sent when starting a session for a local connection failed.
+     *
+     * @type {(undefined | never)}
+     * @memberof IEventNamePropertyMapping
+     */
+    [Telemetry.StartSessionFailedJupyter]: undefined | never;
 }
