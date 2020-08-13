b7475bcd687c93488d7008a32256359fc0ae2693
rchiodo <rchiodo@users.noreply.github.com>
2020-08-13 13:59:16 -0700

    Change to junit output

--- build/reporters/junit-spec.js
+++ build/reporters/junit-spec.js
@@ -1,22 +1,24 @@
 // This code was copied from https://github.com/mochajs/mocha but modified to allow spec output at the same time
-// as the file.
+// as the file and to be in the junit format that we use in AZDO
+
 // Note: Even when running in parallel, the output logger is sync'd. It runs in the root mocha process and not
 // in the same process as the tests.
 'use strict';
 /**
- * @module XUnit
+ * @module JUnit
  */
 /**
  * Module dependencies.
  */
 
-var Base = require('../../node_modules/mocha/lib/reporters/base');
-var utils = require('../../node_modules/mocha/lib/utils');
+var Base = require('mocha/lib/reporters/base');
+var utils = require('mocha/lib/utils');
 var fs = require('fs');
 var path = require('path');
-var errors = require('../../node_modules/mocha/lib/errors');
+var errors = require('mocha/lib/errors');
+const { clear } = require('winston');
 var createUnsupportedError = errors.createUnsupportedError;
-var constants = require('../../node_modules/mocha/lib/runner').constants;
+var constants = require('mocha/lib/runner').constants;
 var EVENT_TEST_PASS = constants.EVENT_TEST_PASS;
 var EVENT_TEST_FAIL = constants.EVENT_TEST_FAIL;
 var EVENT_RUN_END = constants.EVENT_RUN_END;
@@ -24,7 +26,7 @@ var EVENT_TEST_PENDING = constants.EVENT_TEST_PENDING;
 var EVENT_RUN_BEGIN = constants.EVENT_RUN_BEGIN;
 var EVENT_SUITE_BEGIN = constants.EVENT_SUITE_BEGIN;
 var EVENT_SUITE_END = constants.EVENT_SUITE_END;
-var STATE_FAILED = require('../../node_modules/mocha/lib/runnable').constants.STATE_FAILED;
+var STATE_FAILED = require('mocha/lib/runnable').constants.STATE_FAILED;
 var inherits = utils.inherits;
 var escape = utils.escape;
 var color = Base.color;
@@ -35,13 +37,13 @@ var color = Base.color;
 var Date = global.Date;
 
 /**
- * Expose `XUnit`.
+ * Expose `JUnit`.
  */
 
-exports = module.exports = XUnit;
+exports = module.exports = JUnit;
 
 /**
- * Constructs a new `XUnit` reporter instance.
+ * Constructs a new `JUnit` reporter instance.
  *
  * @public
  * @class
@@ -50,11 +52,14 @@ exports = module.exports = XUnit;
  * @param {Runner} runner - Instance triggers reporter actions.
  * @param {Object} [options] - runner options
  */
-function XUnit(runner, options) {
+function JUnit(runner, options) {
     Base.call(this, runner, options);
 
-    var stats = this.stats;
+    // Save root suite for output later
+    this.rootSuite = runner.suite;
+
     var tests = [];
+    var suites = [];
     var self = this;
     var indents = 0;
     var n = 0;
@@ -63,11 +68,8 @@ function XUnit(runner, options) {
         return Array(indents).join('  ');
     }
 
-    // the name of the test suite, as it will appear in the resulting XML file
-    var suiteName;
-
     // the default name of the test suite if none is provided
-    var DEFAULT_SUITE_NAME = 'Mocha Tests';
+    this.suiteName = 'Mocha Tests';
 
     let outputPath = process.env.MOCHA_FILE;
     if (options && options.reporterOptions) {
@@ -75,7 +77,7 @@ function XUnit(runner, options) {
             outputPath = options.reporterOptions.output;
         }
         // get the suite name from the reporter options (if provided)
-        suiteName = options.reporterOptions.suiteName;
+        this.suiteName = options.reporterOptions.suiteName;
     }
     if (outputPath) {
         if (!fs.createWriteStream) {
@@ -87,9 +89,6 @@ function XUnit(runner, options) {
         });
         self.fileStream = fs.createWriteStream(outputPath);
     }
-
-    // fall back to the default suite name
-    suiteName = suiteName || DEFAULT_SUITE_NAME;
 
     runner.on(EVENT_TEST_PENDING, function (test) {
         tests.push(test);
@@ -119,27 +118,7 @@ function XUnit(runner, options) {
     });
 
     runner.once(EVENT_RUN_END, function () {
-        self.write(
-            tag(
-                'testsuite',
-                {
-                    name: suiteName,
-                    tests: stats.tests,
-                    failures: 0,
-                    errors: stats.failures,
-                    skipped: stats.tests - stats.failures - stats.passes,
-                    timestamp: new Date().toUTCString(),
-                    time: stats.duration / 1000 || 0
-                },
-                false
-            )
-        );
-
-        tests.forEach(function (t) {
-            self.test(t);
-        });
-
-        self.write('</testsuite>');
+        self.outputXml(suites, tests);
 
         // Print out the spec output
         self.epilogue();
@@ -150,6 +129,7 @@ function XUnit(runner, options) {
     });
 
     runner.on(EVENT_SUITE_BEGIN, function (suite) {
+        suites.push(suite);
         ++indents;
         Base.consoleLog(color('suite', '%s%s'), indent(), suite.title);
     });
@@ -165,7 +145,7 @@ function XUnit(runner, options) {
 /**
  * Inherit from `Base.prototype`.
  */
-inherits(XUnit, Base);
+inherits(JUnit, Base);
 
 /**
  * Override done to close the stream (if it's a file).
@@ -173,7 +153,7 @@ inherits(XUnit, Base);
  * @param failures
  * @param {Function} fn
  */
-XUnit.prototype.done = function (failures, fn) {
+JUnit.prototype.done = function (failures, fn) {
     if (this.fileStream) {
         this.fileStream.end(function () {
             fn(failures);
@@ -188,7 +168,7 @@ XUnit.prototype.done = function (failures, fn) {
  *
  * @param {string} line
  */
-XUnit.prototype.write = function (line) {
+JUnit.prototype.write = function (line) {
     if (this.fileStream) {
         this.fileStream.write(line + '\n');
     } else if (typeof process === 'object' && process.stdout) {
@@ -198,36 +178,141 @@ XUnit.prototype.write = function (line) {
     }
 };
 
-/**
- * Output tag for the given `test.`
- *
- * @param {Test} test
- */
-XUnit.prototype.test = function (test) {
-    var attrs = {
-        classname: test.parent.fullTitle(),
-        name: test.title,
-        time: test.duration / 1000 || 0
+JUnit.prototype.outputXml = function (suites, tests) {
+    // Turn off colorization. This makes unicode characters that can't be read by
+    // JUnit parsers
+    Base.useColors = false;
+
+    // Write the starting tag for the root
+    this.write(
+        tag(
+            'testsuite',
+            {
+                name: this.suiteName,
+                tests: this.stats.tests,
+                failures: 0,
+                errors: this.stats.failures,
+                skipped: this.stats.tests - this.stats.failures - this.stats.passes,
+                timestamp: new Date().toUTCString(),
+                time: this.stats.duration / 1000 || 0
+            },
+            false
+        )
+    );
+
+    // Clear all of the tests and suites all the way down on the root
+    // We want to just have the ones that actually ran
+    var clearTestsAndSuites = (suite) => {
+        suite.tests = [];
+        suite.suites.forEach(clearTestsAndSuites);
+        suite.suites = [];
+        suite.duration = 0;
+        suite.failures = 0;
     };
+    clearTestsAndSuites(this.rootSuite);
+    this.rootSuite.tests = [...tests];
+
+    Base.consoleLog(JSON.stringify(tests));
+    Base.consoleLog(JSON.stringify(suites));
+
+    // Collect the suites that actually ran up to the root
+    tests.forEach((t) => {
+        // Modify the parents of the test to just be the ones we ran and
+        // add appropriate data to compute our xml later
+        var parent = t.parent;
+        while (parent) {
+            var grandParent = parent.parent;
+
+            // In parallel mode tests are not created in the root
+            if (!parent.tests) {
+                parent.tests = [];
+            }
+            parent.tests.push(t);
+            parent.duration += t.duration;
+            parent.failures += t.state === STATE_FAILED ? 1 : 0;
+
+            // In parallel mode suites are not created in the root.
+            if (grandParent && !grandParent.suites) {
+                grandParent.suites = [];
+            }
+            if (grandParent && !grandParent.suites.find((p) => p == parent)) {
+                grandParent.suites.push(parent);
+            }
+            parent = grandParent;
+        }
+    });
+
+    // Recurse through sub suites and tests
+    if (this.rootSuite.suites && this.rootSuite.suites.length) {
+        this.rootSuite.suites.forEach(this.outputSuite.bind(this));
+    } else if (this.rootSuite.tests && this.rootSuite.tests.length) {
+        this.rootSuite.tests.forEach(this.outputTest.bind(this));
+    }
+
+    this.write('</testsuite>');
+
+    Base.useColors = true;
+};
+
+JUnit.prototype.outputSuite = function (suite) {
+    // Write tag for the suite
+    this.write(
+        tag(
+            'testsuite',
+            {
+                name: suite.title,
+                tests: suite.tests.length,
+                failures: suite.failures,
+                timestamp: new Date().toUTCString(),
+                time: suite.duration / 1000 || 0
+            },
+            false
+        )
+    );
+
+    // Recurse through sub suites and tests
+    if (suite.suites && suite.suites.length) {
+        suite.suites.forEach(this.outputSuite.bind(this));
+    } else if (suite.tests && suite.tests.length) {
+        suite.tests.forEach(this.outputTest.bind(this));
+    }
+
+    this.write('</testsuite>');
+};
+
+JUnit.prototype.outputTest = function (test) {
+    // Write tag for the test case
+    this.write(
+        tag(
+            'testcase',
+            {
+                name: test.fullTitle(),
+                classname: test.title,
+                time: test.duration / 1000 || 0
+            },
+            false
+        )
+    );
 
     if (test.state === STATE_FAILED) {
-        Base.useColors = false;
         var err = test.err;
         var diff = !Base.hideDiff && Base.showDiff(err) ? '\n' + Base.generateDiff(err.actual, err.expected) : '';
+
         this.write(
             tag(
-                'testcase',
-                attrs,
-                false,
-                tag('failure', {}, false, escape(err.message) + escape(diff) + '\n' + escape(err.stack))
+                'failure',
+                {
+                    message: escape(err.message),
+                    type: err.type
+                },
+                false
             )
         );
-        Base.useColors = true;
-    } else if (test.isPending()) {
-        this.write(tag('testcase', attrs, false, tag('skipped', {}, true)));
-    } else {
-        this.write(tag('testcase', attrs, true));
+        this.write(`<![CDATA[${err.message}\n${diff}\n${err.stack}]]>`);
+        this.write('</failure>');
     }
+
+    this.write('</testcase>');
 };
 
 /**
@@ -257,4 +342,4 @@ function tag(name, attrs, close, content) {
     return tag;
 }
 
-XUnit.description = 'XUnit-compatible XML output';
+JUnit.description = 'JUnit-compatible XML output';
